2b7b31d17b232fdce04b6481439f0319a901886b
Jade <jade.phaneuf@gmail.com>
2020-05-08 14:48:15 -0400

    5.2.3 DbContext avec les DbSet

---
+++ Inventaire/BillingManagementContext.cs
@@ -0,0 +1,20 @@
+﻿using BillingManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BillingManagement.UI
+{
+    class BillingManagementContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite("Data Source = BillingManagement.db");
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<ContactInfo> ContactInfos { get; set; }
+    }
+}
