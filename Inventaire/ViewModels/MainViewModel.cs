49102bb12141adebfc7300a8b13a5dc15a72163c
Jade <jade.phaneuf@gmail.com>
2020-05-14 20:35:35 -0400

    5.2.5 init de BD and fills it

--- Inventaire/ViewModels/MainViewModel.cs
+++ Inventaire/ViewModels/MainViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using BillingManagement.Business;
 using BillingManagement.Models;
 using BillingManagement.UI.ViewModels.Commands;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data.Common;
 using System.Linq;
 using System.Windows;
 
@@ -11,10 +13,13 @@ namespace BillingManagement.UI.ViewModels
 	class MainViewModel : BaseViewModel
     {
 		private BaseViewModel _vm;
-		BillingManagementContext db = new BillingManagementContext();
-		ObservableCollection<Customer> dbCustomers;
-		ObservableCollection<Invoice> dbInvoices;
+		private string searchCriteria;
+		private BillingManagementContext db;
+		private ObservableCollection<Customer> customers;
+		private ObservableCollection<Invoice> invoices;
 		readonly CustomersDataService customersDataService = new CustomersDataService();
+		CustomerViewModel customerViewModel;
+		InvoiceViewModel invoiceViewModel;
 
 		public BaseViewModel VM
 		{
@@ -25,8 +30,6 @@ namespace BillingManagement.UI.ViewModels
 			}
 		}
 
-		private string searchCriteria;
-
 		public string SearchCriteria
 		{
 			get { return searchCriteria; }
@@ -36,22 +39,51 @@ namespace BillingManagement.UI.ViewModels
 			}
 		}
 
+		public BillingManagementContext Db 
+		{
+			get => db;
+			set
+			{
+				db = value;
+				OnPropertyChanged();
+			} 
+		}
 
-		CustomerViewModel customerViewModel;
-		InvoiceViewModel invoiceViewModel;
+		public ObservableCollection<Customer> Customers
+		{
+			get => customers;
+			set 
+			{ 
+				customers = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public ObservableCollection<Invoice> Invoices
+		{
+			get => invoices;
+			set
+			{
+				invoices = value;
+				OnPropertyChanged();
+			}
+		}
+
 
 		public ChangeViewCommand ChangeViewCommand { get; set; }
-
 		public DelegateCommand<object> AddNewItemCommand { get; private set; }
-
 		public DelegateCommand<Invoice> DisplayInvoiceCommand { get; private set; }
 		public DelegateCommand<Customer> DisplayCustomerCommand { get; private set; }
-
 		public DelegateCommand<Customer> AddInvoiceToCustomerCommand { get; private set; }
 		public RelayCommand<Customer> SearchCommand { get; set; }
 
+
 		public MainViewModel()
 		{
+			db = new BillingManagementContext();
+
+			fillDB();
+
 			ChangeViewCommand = new ChangeViewCommand(ChangeView);
 			DisplayInvoiceCommand = new DelegateCommand<Invoice>(DisplayInvoice);
 			DisplayCustomerCommand = new DelegateCommand<Customer>(DisplayCustomer);
@@ -64,11 +96,8 @@ namespace BillingManagement.UI.ViewModels
 			customerViewModel = new CustomerViewModel();
 			invoiceViewModel = new InvoiceViewModel(customerViewModel.Customers);
 
-			dbCustomers = new ObservableCollection<Customer>();
-			dbInvoices = new ObservableCollection<Invoice>();
-
-			var sort = dbCustomers.OrderBy(x => x.LastName);
-			var CustomersSorted = new ObservableCollection<Customer>(sort);
+			customers = new ObservableCollection<Customer>();
+			invoices = new ObservableCollection<Invoice>();
 			
 
 			VM = customerViewModel;
@@ -128,27 +157,88 @@ namespace BillingManagement.UI.ViewModels
 		private void SearchCustomer(object parameter)
 		{
 			string input = searchCriteria as string;
-			IEnumerable<Customer> Customers = customerViewModel.Customers.ToList<Customer>();
-			IEnumerable<Customer> FoundCustomers = customerViewModel.Customers.ToList<Customer>();
-			Customer SelectedCustomer = customerViewModel.SelectedCustomer;
-
-			FoundCustomers = Customers.Where(c => c.Name.ToUpper().StartsWith(input.ToUpper()) || c.LastName.ToUpper().StartsWith(input.ToUpper()));
-
-
-			int list = FoundCustomers.Count();
-			if(list > 0)
+			int output;
+			List<Customer> FoundCustomers = new List<Customer>();
+			Customer SelectedCustomer = new Customer();
+			if (Int32.TryParse(input,out output))
 			{
-				customerViewModel.Customers = new ObservableCollection<Customer>(FoundCustomers);
-				customerViewModel.SelectedCustomer = FoundCustomers.First();
+				Customers.Clear();
+				customerViewModel.SelectedCustomer = db.Customers.Find(output);
+				if (customerViewModel.SelectedCustomer != null)
+				{
+					Customers.Add(customerViewModel.SelectedCustomer);
+				}
+				else
+				{
+					MessageBox.Show("Aucun client avec cet ID");
+				}
 			}
 			else
 			{
-				customerViewModel.Customers = new ObservableCollection<Customer> (customersDataService.GetAll().ToList());
-				customerViewModel.SelectedCustomer = Customers.First<Customer>();
-				MessageBox.Show("Aucun contact trouvé, retour à la liste initiale");
-				
+				FoundCustomers = db.Customers.Where(c => c.Name.ToUpper().StartsWith(input.ToUpper()) || c.LastName.ToUpper().StartsWith(input.ToUpper())).ToList();
+				Customers.Clear();
+
+				if (FoundCustomers.Count > 0)
+				{
+					foreach (Customer c in FoundCustomers)
+					{
+						Customers.Add(c);
+					}
+					Customers = new ObservableCollection<Customer>(Customers.OrderBy(c => c.LastName));
+					customerViewModel.Customers = Customers;
+					customerViewModel.SelectedCustomer = Customers.First();
+				}
+				else
+				{
+					customerViewModel.Customers = new ObservableCollection<Customer>(customersDataService.GetAll().ToList());
+					customerViewModel.SelectedCustomer = Customers.First<Customer>();
+					MessageBox.Show("Aucun contact trouvé, retour à la liste initiale");
+				}
+
 			}
 		}
+		
+		private void fillDB()
+		{
+			if (db.Customers.Count() <= 0)
+			{
+				List<Customer> Customers = new CustomersDataService().GetAll().ToList();
+				List<Invoice> Invoices = new InvoicesDataService(Customers).GetAll().ToList();
 
+				foreach (Customer c in Customers)
+				{
+					db.Customers.Add(c);
+					db.SaveChanges();
+				}
+
+			}
+
+		}
+
+		public void GetDataDB(object item)
+		{
+			List<Customer> CustomersList = db.Customers.ToList();
+			Customers.Clear();
+			foreach (Customer c in CustomersList)
+				Customers.Add(c);
+
+			Customers = new ObservableCollection<Customer>(Customers.OrderBy(c => c.LastName));
+
+			if (customerViewModel != null) customerViewModel.Customers = Customers;
+			if (customerViewModel != null) customerViewModel.SelectedCustomer = Customers.First();
+
+
+
+			//Invoices
+			List<Invoice> InvoicesList = db.Invoices.ToList();
+			Invoices.Clear();
+			foreach (Invoice i in InvoicesList)
+				Invoices.Add(i);
+			if (invoiceViewModel != null) invoiceViewModel.SelectedInvoice = Invoices.First();
+		}
 	}
+
 }
+
+
+
